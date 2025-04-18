478574241601171ddb0c9329273279752f717a37
NEOTERICMEDICO <neotericmedico@gmail.com>
2025-04-18 18:50:26 +0530

    practice folder

---
+++ 2. Linear Regression/matplotlib.py
@@ -0,0 +1,7 @@
+import numpy as np
+import matplotlib.pyplot as plt
+
+
+x = (-3,1,0.5,3.2,6.4,-2.8,16,-4.2)
+y= (-61, 9.8, 6.2,18.5, 30.8, -4.9, 12.1,-10.1)
+plt.scatter(x,y)
---
+++ practice/Class2.py
@@ -0,0 +1,8 @@
+import matplotlib.pyplot as plt
+import numpy as np
+
+x = (-4.2, -3, -2.8, 0.5, 1, 1.6, 3.2, 6.4)
+y = (-10.1, -6.1, -4.9, 6.2, 9.8, 12.1, 18.5, 30.8)
+plt.scatter(x, y, color='red', marker='o')
+
+plt.title('Scatter Plot') 
